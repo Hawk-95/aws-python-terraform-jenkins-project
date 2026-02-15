88e47b67a0ba5bab63c9b9d5f07779f5a10314de
Hawk-95 <68895112+Hawk-95@users.noreply.github.com>
2026-02-15 20:27:58 +0530

    Update main.tf

--- terraform/terraform-network/main.tf
+++ terraform/terraform-network/main.tf
@@ -2,7 +2,8 @@ module "infra" {
   source = "./infra"
 
   vpc_cidr               = var.vpc_cidr
-  public_subnet_cidr     = var.public_subnet_cidr
+  public_subnet_1_cidr   = var.public_subnet_1_cidr
+  public_subnet_2_cidr   = var.public_subnet_2_cidr
   private_subnet_1_cidr  = var.private_subnet_1_cidr
   private_subnet_2_cidr  = var.private_subnet_2_cidr
 
