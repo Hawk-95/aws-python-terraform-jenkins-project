3a31256aafee92c039c73dbc572cbc106b9b2eff
Hawk-95 <68895112+Hawk-95@users.noreply.github.com>
2026-02-15 20:23:53 +0530

    Update vpc.tf

--- terraform/terraform-network/infra/vpc.tf
+++ terraform/terraform-network/infra/vpc.tf
@@ -10,14 +10,25 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidr
+  cidr_block              = var.public_subnet_1_cidr
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
-  tags = { Name = "public-subnet" }
+  tags = { Name = "public-subnet-1" }
 }
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "public-subnet-2" }
+}
+
+
 
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.this.id
@@ -43,7 +54,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_1.id
 
   depends_on = [aws_internet_gateway.igw]
 }
@@ -58,8 +69,13 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
 
