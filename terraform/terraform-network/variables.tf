feea8a0e5fe0c93fc1022bd5ae6dc2eef91a4c70
Hawk-95 <68895112+Hawk-95@users.noreply.github.com>
2026-02-15 20:29:48 +0530

    Update variables.tf

--- terraform/terraform-network/variables.tf
+++ terraform/terraform-network/variables.tf
@@ -7,8 +7,12 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_1_cidr" {
   default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  default = "10.0.4.0/24"
 }
 
 variable "private_subnet_1_cidr" {
